e6b21bc7e03b522d64e6831eb2f66ee72e514d3e
Markus Dickscheit <mdickscheit@googlemail.com>
2026-07-13 14:55:28 +0200

    Changes to be committed: 	modified:   Jarvis.bat 	modified:   Start-Jarvis-GUI.bat
     Changes not staged for commit:
    	modified:   Jarvis.bat
    	modified:   Start-Jarvis-GUI.bat
    	modified:   web/jarvis_hud/index.html

--- Jarvis.bat
+++ Jarvis.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-REM Startet die richtige GUI: Web-Control-Center (http://127.0.0.1:8851).
-set "PY=python"
-if exist "%~dp0.venv\Scripts\python.exe" set "PY=%~dp0.venv\Scripts\python.exe"
-"%PY%" launcher.py hud %*
-if errorlevel 1 pause
+py -3 launcher.py jarvis
+if errorlevel 1 (
+  python launcher.py jarvis
+)
+endlocal
--- Start-Jarvis-GUI.bat
+++ Start-Jarvis-GUI.bat
@@ -1,8 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-REM Startet die richtige GUI: Web-Control-Center (http://127.0.0.1:8851).
-set "PY=python"
-if exist "%~dp0.venv\Scripts\python.exe" set "PY=%~dp0.venv\Scripts\python.exe"
-"%PY%" launcher.py hud %*
-if errorlevel 1 pause
+python launcher.py native-gui
+endlocal
